4e7856054fa27a4b377737d32f157fc3f7252ec7
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-03-20 16:23:48 +0530

    Added:No duplicate contact can be added

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const myAddressBook = new AddressBook();
 
 const contact1 = new Contact("Ayushi", "Singh", "123 MG Road", "Delhi", "Delhi", "110001", "9876543210", "ayushi@example.com");
 const contact2 = new Contact("Ashutosh", "Sharma", "456 Lajpat Nagar", "Mumbai", "Maharashtra", "400001", "9876543211", "ashutosh@example.com");
+const duplicateContact = new Contact("Ayushi", "Singh", "789 Andheri", "Mumbai", "Maharashtra", "400002", "9876543212", "ayushi2@example.com");
 
 myAddressBook.addContact(contact1);
 myAddressBook.addContact(contact2);
+myAddressBook.addContact(duplicateContact); // This should be rejected
 
 myAddressBook.displayContacts();
-
-// Count Contacts using reduce()
 myAddressBook.countContacts();
